b31a9e0a2814c6d83f5a95e06f8a83568bd70dc3
Rishabh Sharma <sharma47@buffalo.edu>
2020-07-21 08:42:13 -0700

    add dynamic os username in terraform init command

--- modules/terraform/init.go
+++ modules/terraform/init.go
@@ -3,6 +3,8 @@ package terraform
 import (
 	"fmt"
 
+	"os/user"
+
 	"github.com/gruntwork-io/terratest/modules/testing"
 )
 
@@ -34,7 +36,12 @@ func CustomInit(t testing.TestingT, options *Options) string {
 // CustomInitE function that calls terraform init with custom azurerm plugin and returns stdout/stderr.
 // For now, manually change <user> in the directory path defined for the -plugin-dir flag below, in the args string.
 func CustomInitE(t testing.TestingT, options *Options) (string, error) {
-	args := []string{"init", fmt.Sprintf("-plugin-dir=%s", "/home/rishabh/go/bin"), fmt.Sprintf("-plugin-dir=%s", "/usr/local/bin"), fmt.Sprintf("-plugin-dir=%s", ".terraform/plugins/linux_amd64"), fmt.Sprintf("-plugin-dir=%s", "/home/rishabh/.terraform.d/plugins"), fmt.Sprintf("-upgrade=%t", options.Upgrade)}
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	args := []string{"init", fmt.Sprintf("-plugin-dir=/home/%s/go/bin", user.Username), fmt.Sprintf("-plugin-dir=%s", "/usr/local/bin"), fmt.Sprintf("-plugin-dir=%s", ".terraform/plugins/linux_amd64"), fmt.Sprintf("-plugin-dir=/home/%s/.terraform.d/plugins", user.Username), fmt.Sprintf("-upgrade=%t", options.Upgrade)}
 	args = append(args, FormatTerraformBackendConfigAsArgs(options.BackendConfig)...)
 	return RunTerraformCommandE(t, options, args...)
 }
