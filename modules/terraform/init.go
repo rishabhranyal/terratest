f99542160733496629bdf8777c4b2eece9a84cd5
Rishabh Sharma <sharma47@buffalo.edu>
2020-07-21 02:01:55 -0700

    add custom azurerm plugin in terraform init command

---
+++ modules/terraform/init.go
@@ -0,0 +1,40 @@
+package terraform
+
+import (
+	"fmt"
+
+	"github.com/gruntwork-io/terratest/modules/testing"
+)
+
+// Init calls terraform init and return stdout/stderr.
+func Init(t testing.TestingT, options *Options) string {
+	out, err := InitE(t, options)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return out
+}
+
+// InitE calls terraform init and return stdout/stderr.
+func InitE(t testing.TestingT, options *Options) (string, error) {
+	args := []string{"init", fmt.Sprintf("-upgrade=%t", options.Upgrade)}
+	args = append(args, FormatTerraformBackendConfigAsArgs(options.BackendConfig)...)
+	return RunTerraformCommandE(t, options, args...)
+}
+
+// CustomInit function that calls terraform init with custom azurerm plugin and returns stdout/stderr
+func CustomInit(t testing.TestingT, options *Options) string {
+	out, err := CustomInitE(t, options)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return out
+}
+
+// CustomInitE function that calls terraform init with custom azurerm plugin and returns stdout/stderr.
+// For now, manually change <user> in the directory path defined for the -plugin-dir flag below, in the args string.
+func CustomInitE(t testing.TestingT, options *Options) (string, error) {
+	args := []string{"init -plugin-dir=/home/rishabh/go/bin -plugin-dir=/usr/local/bin -plugin-dir=.terraform/plugins/linux_amd64 -plugin-dir=/home/rishabh/.terraform.d/plugins", fmt.Sprintf("-upgrade=%t", options.Upgrade)}
+	args = append(args, FormatTerraformBackendConfigAsArgs(options.BackendConfig)...)
+	return RunTerraformCommandE(t, options, args...)
+}
