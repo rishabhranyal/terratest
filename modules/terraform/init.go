f2fe120ab0d7297dbb17ae0b853b273b29720a4e
Rishabh Sharma <sharma47@buffalo.edu>
2020-07-21 03:18:05 -0700

    change args for terraform init command

--- modules/terraform/init.go
+++ modules/terraform/init.go
@@ -34,7 +34,7 @@ func CustomInit(t testing.TestingT, options *Options) string {
 // CustomInitE function that calls terraform init with custom azurerm plugin and returns stdout/stderr.
 // For now, manually change <user> in the directory path defined for the -plugin-dir flag below, in the args string.
 func CustomInitE(t testing.TestingT, options *Options) (string, error) {
-	args := []string{"init", fmt.Sprintf("-plugin-dir=%s", "/home/rishabh/go/bin"), fmt.Sprintf("-upgrade=%t", options.Upgrade)}
+	args := []string{"init", fmt.Sprintf("-plugin-dir=%s", "/home/rishabh/go/bin"), fmt.Sprintf("-plugin-dir=%s", "/usr/local/bin"), fmt.Sprintf("-plugin-dir=%s", ".terraform/plugins/linux_amd64"), fmt.Sprintf("-plugin-dir=%s", "/home/rishabh/.terraform.d/plugins"), fmt.Sprintf("-upgrade=%t", options.Upgrade)}
 	args = append(args, FormatTerraformBackendConfigAsArgs(options.BackendConfig)...)
 	return RunTerraformCommandE(t, options, args...)
 }
